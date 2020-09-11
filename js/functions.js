edf814a02295369ecce11b7cb543017cf21c956f
javis920320 <javis9203@gmail.com>
2020-09-11 10:40:35 -0500

    Agregando vue y tailwindcss
    Se agrega vue y tailwindcss para manejo de estilos y SPA

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,39 @@
-const a = "CAdena de Pruebas";
-document.getElementById('prueba').innerText = a;
+
+/*document.addEventListener('DOMContentLoaded',function(){
+   
+
+   const lista=  document.querySelector('.lista');
+
+   let Back=['Javascript','PHP','Codeigniter','Laravel ','Nest Js'];
+   let front=['Angular','Vue','Bostrap','jquery','Wordpress'];
+
+  for (let index = 0; index < Back.length; index++) {
+    lista.innerHTML += Back[index]+'<br>';
+      
+  }
+
+    //lista.innerHTML='<h1>hOLA muNDO '+Back[2]+'</h1>'
+    
+})*/
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Skills',
+        backend:[
+            {text:'Java'},
+            {text:'PHP'},
+            {text:'Codeigniter'},
+            {text:'Laravel'},
+            {text:'Nest js'}
+        ],
+        frontend:[
+            {text:'Javascript'},
+            {text:'Angular'},
+            {text:'Vue'},
+            {text:'jquery'},
+            {text:'Boostrap'}
+        ]
+
+    }
+  })
